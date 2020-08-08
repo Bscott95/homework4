9bddd284d9f38bbe83f41b88d3dd4a322e866f08
Brandon Scott <btfs95@gmail.com>
2020-08-08 14:22:12 -0400

    added questions

--- Assets/script1.js
+++ Assets/script1.js
@@ -60,54 +60,54 @@ const myQuestions = [
         },
         correctAnswer: "d"
     },
-    // {
-    //     question: "String values must be enclosed within ____ when being assigned to variables",
-    //     answers: {
-    //         a: "commas",
-    //         b: "curly brackets",
-    //         c: "quotes",
-    //         d: "parentheses"
-    //     },
-    //     correctAnswer: "c"
-    // },
-    // {
-    //     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    //     answers: {
-    //         a: "JavaScript",
-    //         b: "termina/bash",
-    //         c: "for loops",
-    //         d: "console.log"
-    //     },
-    //     correctAnswer: "d"
-    // },
-    // {
-    //     question: "Who invented JavaScript?",
-    //     answers: {
-    //         a: "Douglas Crockford",
-    //         b: "Sheryl Sandberg",
-    //         c: "Brendan Eich"
-    //     },
-    //     correctAnswer: "c"
-    // },
-    // {
-    //     question: "Which one of these is a JavaScript package manager?",
-    //     answers: {
-    //         a: "Node.js",
-    //         b: "TypeScript",
-    //         c: "npm"
-    //     },
-    //     correctAnswer: "c"
-    // },
-    // {
-    //     question: "Which tool can you use to ensure code quality?",
-    //     answers: {
-    //         a: "Angular",
-    //         b: "jQuery",
-    //         c: "RequireJS",
-    //         d: "ESLint"
-    //     },
-    //     correctAnswer: "d"
-    // }
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables",
+        answers: {
+            a: "commas",
+            b: "curly brackets",
+            c: "quotes",
+            d: "parentheses"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            a: "JavaScript",
+            b: "termina/bash",
+            c: "for loops",
+            d: "console.log"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Who invented JavaScript?",
+        answers: {
+            a: "Douglas Crockford",
+            b: "Sheryl Sandberg",
+            c: "Brendan Eich"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which one of these is a JavaScript package manager?",
+        answers: {
+            a: "Node.js",
+            b: "TypeScript",
+            c: "npm"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+            a: "Angular",
+            b: "jQuery",
+            c: "RequireJS",
+            d: "ESLint"
+        },
+        correctAnswer: "d"
+    }
 ];
 
 // When the start button is hit, startQuiz function runs
